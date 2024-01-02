4fc5e5bc00750fa54b3a6e721ebcaa196391e724
gongsy1221 <gongsy0724@naver.com>
2024-01-02 19:33:47 +0900

    UI
    UI완성, 먼지 효과

--- GameJam_OTTFF_WatchYourStep/Assets/01.Scripts/Managers/UIManager.cs
+++ GameJam_OTTFF_WatchYourStep/Assets/01.Scripts/Managers/UIManager.cs
@@ -8,11 +8,12 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     [SerializeField] Image settingPanel;
+    [SerializeField] Image gameOverPanel;
 
     public Slider _musicSlider, _sfxSlider;
 
     public float dotTime = 0.5f;
-    
+
     public void ToggleMusic()
     {
         AudioManager.Instance.ToggleMusic();
@@ -33,28 +34,38 @@ public class UIManager : MonoBehaviour
         AudioManager.Instance.SFXVolume(_sfxSlider.value);
     }
 
-    public void SettingButton()
+    public void SettingButton() // 설정창 키기
     {
         settingPanel.rectTransform.DOScale(1, dotTime).SetEase(Ease.InSine);
     }
 
-    public void SettingXButton()
+    public void XButton() // 설정창 끄기
     {
         settingPanel.rectTransform.DOScale(0, dotTime).SetEase(Ease.InSine);
     }
 
-    public void ExitButton()
+    public void ExitButton() // 게임 끄기
     {
         Application.Quit();
     }
 
-    public void HomeButton()
+    public void HomeButton() // 타이틀 화면으로 이동
     {
-        SceneManager.LoadScene("MainScene");
+        // SceneManager.LoadScene("MainScene");
     }
 
-    public void RestartButton()
+    public void RestartButton() // 게임 재시작
     {
-        SceneManager.LoadScene("GameScene");
+        // SceneManager.LoadScene("GameScene");
+    }
+
+    public void OnGameOver() // 게임오버 오픈
+    {
+        gameOverPanel.rectTransform.DOScale(1, dotTime).SetEase(Ease.InSine);
+    }
+
+    public void OffGameOver() // 게임오버 끄기
+    {
+        gameOverPanel.rectTransform.DOScale(0, dotTime).SetEase(Ease.InSine);
     }
 }
