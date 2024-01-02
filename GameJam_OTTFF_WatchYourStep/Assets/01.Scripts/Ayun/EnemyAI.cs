324195e903413620f2de4fdcd66e1ce78923082b
Ayun14 <127943296+Ayun14@users.noreply.github.com>
2024-01-03 01:22:22 +0900

    애니메이션까지 고친 AI 최종

--- GameJam_OTTFF_WatchYourStep/Assets/01.Scripts/Ayun/EnemyAI.cs
+++ GameJam_OTTFF_WatchYourStep/Assets/01.Scripts/Ayun/EnemyAI.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class EnemyAI : MonoBehaviour
 {
@@ -8,96 +11,44 @@ public class EnemyAI : MonoBehaviour
     {
         Live, Death
     }
+
+    public Action<Vector3> onMovingPosition;
+
+    [SerializeField] private float roamingDirChangeTime = 5f;
+    private float randRoamingTime;
+
     private State currentState;
-
-    #region MoveToNextPos
-    [Header("MoveToNextPos")]
-    [SerializeField] private List<Transform> points;
-    [SerializeField] private int nextID = 0;
-    [SerializeField] private float moveDistance = 10;
-    [SerializeField] private float minSpeed = 5;
-    [SerializeField] private float maxSpeed = 25;
-
-    private float currentSpeed;
-    private int _idChangeValue = 1;
-    #endregion
-
-    //private void Reset()
-    //{
-    //    Init();
-    //}
-
-    private void Init()
-    {
-        // root object 생성
-        GameObject root = new GameObject(name + "_Root");
-        root.transform.position = transform.position;
-        transform.SetParent(root.transform);
-
-        GameObject waypoints = new GameObject("Waypoints");
-        waypoints.transform.SetParent(root.transform);
-
-        GameObject p1 = new GameObject("Point1");
-        p1.transform.SetParent(waypoints.transform);
-        GameObject p2 = new GameObject("Point2");
-        p2.transform.SetParent(waypoints.transform);
-
-        p1.transform.position = RandomVector();
-        p2.transform.position = RandomVector();
-
-        while (Vector3.Distance(p1.transform.position, p2.transform.position) < moveDistance)
-        {
-            p1.transform.position = RandomVector();
-            p2.transform.position = RandomVector();
-        }
-
-        points = new List<Transform>();
-        points.Add(p1.transform);
-        points.Add(p2.transform);
-    }
-
-    private Vector3 RandomVector()
-    {
-        return new Vector3(Random.Range(-moveDistance, moveDistance), 0, Random.Range(-moveDistance, moveDistance));
-    }
-
-    private void Awake()
-    {
-        Init();
-    }
+    private Vector3 roamingPosition;
+    private float timeRoaming;
 
     private void Start()
     {
-        currentState = State.Live;
-        currentSpeed = Random.Range(minSpeed, maxSpeed);
+        roamingPosition = GetRoamingPosition();
+
+        randRoamingTime = Random.Range(1f, roamingDirChangeTime);
     }
 
     private void Update()
     {
-        MoveToNextPoint();
+        if (currentState == State.Death) return;
+
+        Roaming();
     }
 
-    private void MoveToNextPoint() // 정해진 부분을 왔다갔다 이동
+    private void Roaming()
     {
-        Transform goalPint = points[nextID];
+        timeRoaming += Time.deltaTime;
 
-        if (goalPint.transform.position.x > transform.position.x)
-            transform.rotation = Quaternion.Euler(transform.rotation.x, 90, transform.rotation.z);
-        else
-            transform.rotation = Quaternion.Euler(transform.rotation.x, -90, transform.rotation.z);
+        onMovingPosition?.Invoke(roamingPosition);
 
-        transform.position = Vector3.MoveTowards(transform.position, goalPint.position, currentSpeed * Time.deltaTime);
+        if (timeRoaming > roamingDirChangeTime)
+            roamingPosition = GetRoamingPosition();
+    }
 
-        if (Vector3.Distance(transform.position, goalPint.position) < 1f)
-        {
-            if (nextID == points.Count - 1)
-                _idChangeValue = -1;
-
-            if (nextID == 0)
-                _idChangeValue = 1;
-
-            nextID += _idChangeValue;
-        }
+    Vector3 GetRoamingPosition()
+    {
+        timeRoaming = 0;
+        return new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f)).normalized;
     }
 
     private void OnCollisionEnter(Collision collision)
