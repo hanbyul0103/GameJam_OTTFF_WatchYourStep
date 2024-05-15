58745ed4b67d5120fc4d7c874ce2391af7ff47e9
all007b <all007b@naver.com>
2024-05-16 00:25:10 +0900

    commit

--- GameJam_OTTFF_WatchYourStep/Assets/01.Scripts/Player/PlayerMovement.cs
+++ GameJam_OTTFF_WatchYourStep/Assets/01.Scripts/Player/PlayerMovement.cs
@@ -23,21 +23,22 @@ public class PlayerMovement : MonoBehaviour
 
     private void LateUpdate()
     {
-        if (!GameManager.Instance.isGameStart) return;
-
-        if (Input.GetMouseButtonDown(0))
+        if (GameManager.Instance.isGameStart)
         {
-            animator.SetBool("isGameStart", true);
-            animator.speed = 1.5f;
-        }
+            if (Input.GetMouseButtonDown(0))
+            {
+                animator.SetBool("isGameStart", true);
+                animator.speed = 1.5f;
+            }
 
-        if (Input.GetMouseButtonUp(0))
-        {
-            animator.speed = 0.3f;
-            movementSpeed = 10.0f;
-        }
+            if (Input.GetMouseButtonUp(0))
+            {
+                animator.speed = 0.3f;
+                movementSpeed = 10.0f;
+            }
 
-        transform.position += Vector3.left * movementSpeed * Time.deltaTime;
+            transform.position += Vector3.left * movementSpeed * Time.deltaTime;
+        }
     }
 
     public void Stop()
